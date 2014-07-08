4aff8a8d39f79b757759d8d1315446b785614ef3
Dwight Guth <dwightguth@runtimeverification.com>
2014-07-08 14:40:09 -0500

    temp

--- src/main/java/org/kframework/utils/maude/MaudeRun.java
+++ src/main/java/org/kframework/utils/maude/MaudeRun.java
@@ -4,17 +4,14 @@ package org.kframework.utils.maude;
 import org.kframework.utils.errorsystem.KException;
 import org.kframework.utils.errorsystem.KException.ExceptionType;
 import org.kframework.utils.errorsystem.KException.KExceptionGroup;
-import org.kframework.utils.file.FileUtil;
 import org.kframework.utils.file.JarInfo;
 import org.kframework.utils.general.GlobalSettings;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MaudeRun {
-
-    static boolean internalMaude = true;
+    
+    private static String MAUDE_DIR = "lib/dependency/maude";
 
     /**
      * This function computes the path to a K-included version of maude. It assumes that /dist/lib/maude directory contains all maude executables. It searches for the os type and the architecture and it returns the right maude executable.
@@ -28,12 +25,10 @@ public class MaudeRun {
         // get system properties: file separator, os name, os architecture
         String fileSeparator = System.getProperty("file.separator");
         String osname = System.getProperty("os.name");
-        String version = System.getProperty("os.version");
         String arch = System.getProperty("os.arch");
 
         // set different maude executables
         String maude_win = "maude.exe";
-        String maude_mac_leopard = "maude.intelDarwinLeopard";
         String maude_mac = "maude.intelDarwin";
         String maude_linux_32 = "maude.linux";
         String maude_linux_64 = "maude.linux64";
@@ -41,27 +36,15 @@ public class MaudeRun {
         // System.out.println("OS: |" + osname + "|" + arch + "|");
         // System.out.println(KPaths.getKBase(true));
 
-        String maudeDir = JarInfo.getKBase(false) + fileSeparator + JarInfo.MAUDE_DIR;
+        String maudeDir = JarInfo.getKBase(false) + fileSeparator + MAUDE_DIR;
         String maudeExe = "maude";
 
 
         if (osname.toLowerCase().contains("win")) {
-            // silently ignore this case
-            // the user should install itself maude
-            // we assume that he can execute maude from command line
             maudeExe = maudeDir + fileSeparator + maude_win;
-        //} else if (osname.toLowerCase().contains("mac")) {
-            // I hope this condition is strong enough
         } else if (osname.equals("Mac OS X")) {
-            String[] parsedVersion = version.split("\\.");
-            String subversion = parsedVersion[1];
-            // if at least Snow Leopard
-            if (Integer.parseInt(subversion) >= 6)
-                maudeExe = maudeDir + fileSeparator + maude_mac;
-            else
-                maudeExe = maudeDir + fileSeparator + maude_mac_leopard;
+            maudeExe = maudeDir + fileSeparator + maude_mac;
         } else if (osname.toLowerCase().contains("linux")) {
-            // in this case we assume linux
             if (arch.toLowerCase().contains("64")) {
                 maudeExe = maudeDir + fileSeparator + maude_linux_64;
             } else
@@ -70,15 +53,10 @@ public class MaudeRun {
 
         final File maude = new File(maudeExe);
         if (!maude.exists()) {
-            KException exception = new KException(ExceptionType.WARNING, KExceptionGroup.CRITICAL,
-                    "Cannot execute Maude from " + maudeExe + ".\n" +
-                            "Will assume that Maude is installed by the user such that\n" +
-                            "it can be executed with no warnings using the 'maude' command.",
-                    "top level", "");
+            KException exception = new KException(ExceptionType.ERROR, KExceptionGroup.INTERNAL,
+                    "Cannot execute Maude from " + maudeExe + ".");
             GlobalSettings.kem.register(exception);
-            // if the maude binaries are not found then consider default `maude`
-            internalMaude = false;
-            return "maude";
+            throw new AssertionError("unreachable");
         } else {
             if (!maude.canExecute()) {
                 maude.setExecutable(true);
